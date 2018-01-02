87e3daf81c33c3a45befc7b761db2ddb4417630b
Paul Csapak <office@webcascade.com>
2018-01-03 00:43:20 +0100

    setup basic example

---
+++ src/index.js
@@ -0,0 +1,14 @@
+const fs = require('fs');
+const markov = require('markov');
+
+const m = markov(1);
+
+const seed = fs.createReadStream(`${__dirname}/../vendor/lyrics.txt`);
+const limit = 10;
+
+m.seed(seed, () => {
+  const randomKey = m.pick();
+  const response = m.fill(randomKey, limit).join(' ');
+  console.log(response);
+});
+
