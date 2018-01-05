43f9098d9522d818f78745be8d3744e0c3074163
Paul Csapak <office@webcascade.com>
2018-01-06 00:07:17 +0100

    fix test

--- test/index.js
+++ test/index.js
@@ -9,7 +9,7 @@ chai.use(chaiAsPromised);
 describe('Response contains', () => {
   it('generated words with random key when no parameters are passed', () => hurn().should.eventually.be.a('string'));
 
-  it('generated words with key passed as parameter', () => hurn('stoli1').should.eventually.be.a('string').that.includes('Stoli'));
+  it('generated words with key passed as parameter', () => hurn('stoli').should.eventually.be.a('string').that.includes('Stoli'));
 
   it('"Key not found" when set key isn\'t available', () => hurn('foobar').should.eventually.be.rejectedWith(Error));
 });
