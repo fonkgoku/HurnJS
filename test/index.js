4e9de1c03baf453da3651c6d0a897ea1ac4f307e
Paul Csapak <office@webcascade.com>
2018-01-06 00:06:55 +0100

    format tests

--- test/index.js
+++ test/index.js
@@ -7,15 +7,9 @@ chai.should();
 chai.use(chaiAsPromised);
 
 describe('Response contains', () => {
-  it('generated words with random key when no parameters are passed', () => {
-    return hurn().should.eventually.be.a('string');
-  });
+  it('generated words with random key when no parameters are passed', () => hurn().should.eventually.be.a('string'));
 
-  it('generated words with key passed as parameter', () => {
-    return hurn('stoli').should.eventually.be.a('string').that.includes('Stoli');
-  });
+  it('generated words with key passed as parameter', () => hurn('stoli1').should.eventually.be.a('string').that.includes('Stoli'));
 
-  it('"Key not found" when set key isn\'t available', () => {
-    return hurn('foobar').should.eventually.be.rejectedWith(Error);
-  });
+  it('"Key not found" when set key isn\'t available', () => hurn('foobar').should.eventually.be.rejectedWith(Error));
 });
